f40f5147a9b48320d51305dfc2ef9bf34043e769
yhkim <kvoyant@gmail.com>
2019-05-18 15:39:36 +0900

    Initial commit

---
+++ app/src/main/java/com/tjeit/baseballgame01/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tjeit.baseballgame01;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tjeit.baseballgame01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
