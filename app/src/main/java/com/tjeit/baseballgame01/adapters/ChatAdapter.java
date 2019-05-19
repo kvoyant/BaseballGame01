0f95528ed0bbee723c8c7587dec55b97279600e4
yhkim <kvoyant@gmail.com>
2019-05-19 10:23:41 +0900

    채팅 내용 출력하기 getView 마무리

--- app/src/main/java/com/tjeit/baseballgame01/adapters/ChatAdapter.java
+++ app/src/main/java/com/tjeit/baseballgame01/adapters/ChatAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.tjeit.baseballgame01.R;
 import com.tjeit.baseballgame01.datas.Chat;
@@ -31,6 +33,26 @@ public class ChatAdapter extends ArrayAdapter<Chat> {
 
         if (row == null) {
             row = inf.inflate(R.layout.chat_list_item, null);
+        }
+
+        Chat data = mList.get(position);
+
+        FrameLayout userMsgFramLayout = row.findViewById(R.id.userMessageFramLayout);
+        FrameLayout comMsgFramLayout = row.findViewById(R.id.computerMessageFramLayout);
+        TextView userMsgTxt = row.findViewById(R.id.userMessageTxt);
+        TextView comMsgTxt = row.findViewById(R.id.computerMessageTxt);
+
+        if(data.userSaid) {
+            userMsgFramLayout.setVisibility(View.VISIBLE);
+            comMsgFramLayout.setVisibility(View.GONE);
+
+            userMsgTxt.setText(data.message);
+        }
+        else {
+            userMsgFramLayout.setVisibility(View.GONE);
+            comMsgFramLayout.setVisibility(View.VISIBLE);
+
+            comMsgTxt.setText(data.message);
         }
 
         return row;
